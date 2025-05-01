3c500bb4e6831f8a9e9e64453d3c1c909c2e743b
Denys Vasylchenko <1duglas1@gmail.com>
2025-05-01 15:19:02 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { randomBytes } from 'node:crypto';
-import { OAuthClient } from './oauth';
+import { randomBytes } from "node:crypto";
+import { OAuthClient } from "./oauth";
 
 interface Env {
 	GITHUB_OAUTH_ID: string;
@@ -11,27 +11,30 @@ const createOAuth = (env: Env) => {
 		id: env.GITHUB_OAUTH_ID,
 		secret: env.GITHUB_OAUTH_SECRET,
 		target: {
-			tokenHost: 'https://github.com',
-			tokenPath: '/login/oauth/access_token',
-			authorizePath: '/login/oauth/authorize',
+			tokenHost: "https://github.com",
+			tokenPath: "/login/oauth/access_token",
+			authorizePath: "/login/oauth/authorize",
 		},
 	});
 };
 
 const handleAuth = async (url: URL, env: Env) => {
-	const provider = url.searchParams.get('provider');
-	if (provider !== 'github') {
-		return new Response('Invalid provider', { status: 400 });
+	const provider = url.searchParams.get("provider");
+	if (provider !== "github") {
+		return new Response("Invalid provider", { status: 400 });
 	}
 
 	const oauth2 = createOAuth(env);
 	const authorizationUri = oauth2.authorizeURL({
 		redirect_uri: `https://${url.hostname}/callback?provider=github`,
-		scope: 'public_repo,user',
-		state: randomBytes(4).toString('hex'),
+		scope: "repo,user",
+		state: randomBytes(4).toString("hex"),
 	});
 
-	return new Response(null, { headers: { location: authorizationUri }, status: 301 });
+	return new Response(null, {
+		headers: { location: authorizationUri },
+		status: 301,
+	});
 };
 
 const callbackScriptResponse = (status: string, token: string) => {
@@ -56,19 +59,19 @@ const callbackScriptResponse = (status: string, token: string) => {
 </head>
 </html>
 `,
-		{ headers: { 'Content-Type': 'text/html' } }
+		{ headers: { "Content-Type": "text/html" } },
 	);
 };
 
 const handleCallback = async (url: URL, env: Env) => {
-	const provider = url.searchParams.get('provider');
-	if (provider !== 'github') {
-		return new Response('Invalid provider', { status: 400 });
+	const provider = url.searchParams.get("provider");
+	if (provider !== "github") {
+		return new Response("Invalid provider", { status: 400 });
 	}
 
-	const code = url.searchParams.get('code');
+	const code = url.searchParams.get("code");
 	if (!code) {
-		return new Response('Missing code', { status: 400 });
+		return new Response("Missing code", { status: 400 });
 	}
 
 	const oauth2 = createOAuth(env);
@@ -76,18 +79,22 @@ const handleCallback = async (url: URL, env: Env) => {
 		code,
 		redirect_uri: `https://${url.hostname}/callback?provider=github`,
 	});
-	return callbackScriptResponse('success', accessToken);
+	return callbackScriptResponse("success", accessToken);
 };
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
 		const url = new URL(request.url);
-		if (url.pathname === '/auth') {
+		if (url.pathname === "/auth") {
 			return handleAuth(url, env);
 		}
-		if (url.pathname === '/callback') {
+		if (url.pathname === "/callback") {
 			return handleCallback(url, env);
 		}
-		return new Response('Hello 👋');
+		return new Response("Hello 👋");
 	},
 };
